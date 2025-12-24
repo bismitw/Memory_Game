01d247d13c1ba3d36e872bffaffef3b37e5aecd6
Bismit Wagle <bismitwagle@gmail.com>
2025-12-24 15:00:02 +0530

    card building shuffling and startNewGame functions added

--- script.js
+++ script.js
@@ -23,10 +23,60 @@ document.addEventListener("DOMContentLoaded",()=>{
     
 
     //Emoji pairs Game Content (8 uniue pairs)
-    const Emojis = ["🦄", "🐸", "🐷", "🐯", "🦋", "🐢", "🦒", "🦩"];
+    const emojis = ["🦄", "🐸", "🐷", "🐯", "🦋", "🐢", "🦒", "🦩"];
 
 
-    
+    //Create single card -Build HTML structure
+    function createCard(emoji , index){
+        const card = document.createElement('div');
+        card.classList.emoji = emoji // stores emoji invisibly
+        card.dataset.index= index ; //position tracker
+        card.innerHTML = `
+            <div class= "card-back">? </div>
+            <div class= "card-front">${emoji}</div>
+
+        `;
+        return card;
+
+    }
+
+    //Shuffle array 
+    //professional card shuffling algorithm.
+    function shuffle(array){
+        const shuffled = [...array]; //... spread operator clones array original array stays unchanged
+        for(let i = shuffled.length-1; i>0; i--){ //starts from last item Each position gets random swap partner
+            const j = math.floor(math.random()*(i+1)); //Random index
+            [shuffled[i], shuffled[j] = shuffled[i], shuffled[j]] //swap
+        }
+        return shuffled;
+
+    }
+
+    //Start new game Full reset 
+    function startNewGame(){
+        gameActive = true; //Allow clicks
+        moves = 0; //reset counter
+        time = 0; //reset timer
+        flippedCards = []; //Clear flipped
+        gameBoard.innerHTML= '' //Empty Board
+        gameOverScreen.classList.add('hidden'); //hide win screen
+        clearInterval(timer); //stop timer
+
+        //create 8 pairs (16 total cards)
+        const gameEmojis = [...emojis.slice(0, cardCount/2), ...emojis.slice(0, cardCount/2)];
+        const shuffledEmojis = shuffle(gameEmojis);
+
+        //Build all cards and Add to board
+        cards = shuffledEmojis.map((emoji, index) => createCard(emoji, index));
+        cards.forEach(card => gameBoard.appendChild(card));
+
+
+        //Reset Displays
+        movesDisplay.textContent = moves;
+        timeDisplay.textContent= '00:00';
+
+    }
+
 
 
 
